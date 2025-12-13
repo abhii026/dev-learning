10b93eb7fc24bb1478aefbc5ef0f16a8b60ff636
Abhishek Singh <iabhii026@gmail.com>
2025-12-13 16:42:45 +0530

    Updated File manegement system project

--- project/FileManagementSystem.py
+++ project/FileManagementSystem.py
@@ -37,20 +37,62 @@ def readFile():
             print("File does not exist.")
     except Exception as err:
         print(f"An error occurred as {err}")
-
-def appendfile():
+def updatefile():
     try:
-        name = input("Enter file name to append: ").strip()
+        name = input("Enter file name to update: ").strip()
         p = BASE_DIR / name
-        if p.exists():
-            with open(p, "a") as fs:
-                data = input("Enter your content: ")
-                fs.write("\n"+data)
-            print("File appended successfully.")
-        else:
+
+        if not p.exists() or not p.is_file():
             print("File does not exist.")
+            return
+
+        while True:
+            print("\n1. Change file name")
+            print("2. Overwrite the data in file")
+            print("3. Append data to file")
+            print("4. Exit")
+
+            try:
+                res = int(input("Select (1 to 4): "))
+            except ValueError:
+                print("Please enter a number only.")
+                continue
+
+            match res:
+                case 1:
+                    name2 = input("Enter new file name: ").strip()
+                    p2 = BASE_DIR / name2
+
+                    if p2.exists():
+                        print("File with this name already exists.")
+                        continue
+
+                    p.rename(p2)
+                    print(f"File renamed from {p.name} to {p2.name}")
+                    p = p2
+
+                case 2:
+                    with open(p, "w") as fs:
+                        data = input("Enter data to overwrite: ")
+                        fs.write(data)
+                    print("Data overwritten successfully.")
+
+                case 3:
+                    with open(p, "a") as fs:
+                        data = input("Enter data to append: ")
+                        fs.write("\n" + data)
+                    print("Data appended successfully.")
+
+                case 4:
+                    print("Exiting update menu...")
+                    break
+
+                case _:
+                    print("Invalid choice")
+
     except Exception as err:
         print(f"An error occurred as {err}")
+
 
 def deletefile():
     try:
@@ -90,7 +132,7 @@ while True:
         case 3:
             readFile()
         case 4:
-            appendfile()
+            updatefile()
         case 5:
             deletefile()
         case 6:
